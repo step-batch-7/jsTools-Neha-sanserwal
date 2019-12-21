f076837ce332e77bc00731c33e2b4800ae36a8ff
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2019-12-21 18:08:21 +0530

    added function parseTailOption

--- src/lib.js
+++ src/lib.js
@@ -1,9 +1,26 @@
+const fs = require("fs");
 const generateTailLines = function(fileContent) {
 	let lines = fileContent.data.split("\n");
 	let slicedLines = lines.reverse().slice(0, 10);
 	return slicedLines.reverse().join("\n");
 };
 
+const loadFileContent = function(tailOption) {
+	data = fs.readFileSync(tailOption.filePath, "utf8");
+	return { data };
+};
+const parseTailOption = function(userOption) {
+	return { filePath: userOption[0] };
+};
+
+const filterUserOption = function(cmdArgs) {
+	let [, , ...userOption] = [...cmdArgs];
+	return userOption;
+};
+
 module.exports = {
-	generateTailLines
+	generateTailLines,
+	loadFileContent,
+	filterUserOption,
+	parseTailOption
 };
---
+++ test/testLib.js
@@ -0,0 +1,50 @@
+const assert = require("chai").assert;
+const {
+	generateTailLines,
+	loadFileContent,
+	filterUserOption,
+	parseTailOption
+} = require("../src/lib");
+
+describe("generateTailLines", function() {
+	it("should give last ten line of file content if lines are more than 10", function() {
+		let data = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11";
+		let fileContent = { data };
+		let expected = "2\n3\n4\n5\n6\n7\n8\n9\n10\n11";
+		assert.strictEqual(generateTailLines(fileContent), expected);
+	});
+	it("should give last total line of file content if lines are less than 10", function() {
+		let data = "1\n2\n3\n4\n5";
+		let fileContent = { data };
+		let expected = "1\n2\n3\n4\n5";
+		assert.strictEqual(generateTailLines(fileContent), expected);
+	});
+});
+
+describe("loadFileContent", function() {
+	let data = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11";
+	let fileContent = { data };
+	let tailOptions = { filePath: "sample.txt" };
+	it("should load file content", function() {
+		assert.deepStrictEqual(loadFileContent(tailOptions), fileContent);
+	});
+});
+
+describe("parseTailOption", function() {
+	it("should do move array of user Option to Object with Valid key", function() {
+		let userOption = ["sample.txt"];
+		let tailOption = { filePath: "sample.txt" };
+		assert.deepStrictEqual(parseTailOption(userOption), tailOption);
+	});
+});
+
+describe("filterUserOption", function() {
+	it("should filter The user Option", function() {
+		let cmdArgs = ["node", "tail.js", "sample.txt"];
+		let userOption = ["sample.txt"];
+		assert.deepStrictEqual(filterUserOption(cmdArgs), userOption);
+		cmdArgs = ["node", "tail.js", "sample.txt", "a", "b"];
+		userOption = ["sample.txt", "a", "b"];
+		assert.deepStrictEqual(filterUserOption(cmdArgs), userOption);
+	});
+});
