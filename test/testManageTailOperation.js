f16284b2a8390219bf8d02c58c2fe55a083f739d
Neha Sanserwal <neha.sanserwal@thoughtworks.com>
2019-12-23 12:55:03 +0530

    added tests for manageTailOperations.

--- test/testManageTailOperation.js
+++ test/testManageTailOperation.js
@@ -1,15 +1,32 @@
 const assert = require("chai").assert;
 const { manageTailOperation } = require("../src/manageTailOperation");
-const { fsModules } = require("../src/config");
 describe("manageTailOperation", function() {
-	it("should give error if can't generate tail lines of given file", function() {
+	it("should give error if can't find given file", function() {
+		const fsModules = {};
+		fsModules.encoding = "utf8";
+		fsModules.fileExist = function(filePath) {
+			assert.strictEqual(filePath, "bad");
+			return false;
+		};
 		let cmdArgs = ["node", "tail.js", "bad"];
 		let expected = "tail: bad: no such file or directory";
 		assert.strictEqual(manageTailOperation(cmdArgs, fsModules), expected);
 	});
 	it("should generate tail lines of given file", function() {
+		const fsModules = {
+			encoding: "utf8"
+		};
+		fsModules.fileExist = function(filePath) {
+			assert.strictEqual(filePath, "sample.txt");
+			return true;
+		};
+		encoding = "utf8";
+		fsModules.readFile = function(filePath, encoding) {
+			assert.strictEqual(filePath, "sample.txt");
+			assert.strictEqual(encoding, "utf8");
+			return "1\n2\n3";
+		};
 		let cmdArgs = ["node", "tail.js", "sample.txt"];
-		let expected = "2\n3\n4\n5\n6\n7\n8\n9\n10\n11";
-		assert.strictEqual(manageTailOperation(cmdArgs, fsModules), expected);
+		assert.strictEqual(manageTailOperation(cmdArgs, fsModules), "1\n2\n3");
 	});
 });
